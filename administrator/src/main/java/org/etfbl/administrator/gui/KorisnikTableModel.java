1b1a11d7d5127063d0f3e74ce212d49b4791e6c5
Aleksandra Vucicevic <vucicevicaleksandra03@gmail.com>
2025-10-23 19:12:03 +0200

    Implemented class KorisnikTableModel

---
+++ administrator/src/main/java/org/etfbl/administrator/gui/KorisnikTableModel.java
@@ -0,0 +1,56 @@
+package org.etfbl.administrator.gui;
+
+import org.etfbl.administrator.model.Korisnik;
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class KorisnikTableModel extends AbstractTableModel {
+    private final String[] kolone = {"JMB", "Ime", "Prezime", "Username", "Email", "Broj telefona"};
+    private List<Korisnik> korisnici;
+
+    public KorisnikTableModel(List<Korisnik> korisnici) {
+        setPodaci(korisnici);
+    }
+
+    public void setPodaci(List<Korisnik> podaci) {
+        this.korisnici = podaci;
+    }
+
+    public Korisnik getKorisnikAtRow(int rowIndex) {
+        return korisnici.get(rowIndex);
+    }
+
+    @Override
+    public int getColumnCount() {
+        return kolone.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return kolone[column];
+    }
+
+    @Override
+    public int getRowCount() {
+        return korisnici.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Korisnik k = korisnici.get(rowIndex);
+        if (columnIndex == 0)
+            return k.getJmb();
+        else if (columnIndex == 1)
+            return k.getIme();
+        else if (columnIndex == 2)
+            return k.getPrezime();
+        else if (columnIndex == 3)
+            return k.getUsername();
+        else if (columnIndex == 4)
+            return k.getEmail();
+        else if (columnIndex == 5)
+            return k.getBrojTelefona();
+        else
+            return null;
+    }
+}
