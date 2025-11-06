8e03e4ecdf77e2174f4c8da66f41a7b258877501
jelena-maletic <jelenamaletic03@gmail.com>
2025-11-06 18:06:32 +0100

    Changed return type in findByUsername in KorisnikRepository

--- backend/src/main/java/org/etfbl/backend/repository/KorisnikRepository.java
+++ backend/src/main/java/org/etfbl/backend/repository/KorisnikRepository.java
@@ -3,9 +3,9 @@ package org.etfbl.backend.repository;
 import org.etfbl.backend.model.Korisnik;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface KorisnikRepository extends JpaRepository<Korisnik, String> {
-    List<Korisnik> findByUsername(String username);
+    Optional<Korisnik> findByUsername(String username);
 
 }
