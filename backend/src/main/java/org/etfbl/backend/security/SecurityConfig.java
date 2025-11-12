595f18208bde020ecebaf5702fa3aeae72da1668
Aleksandra Vucicevic <vucicevicaleksandra03@gmail.com>
2025-11-12 21:24:05 +0100

    Update SecurityConfig to allow access to /api/projekti endpoint

--- backend/src/main/java/org/etfbl/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/etfbl/backend/security/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login/**").permitAll() // login endpoint
+                        .requestMatchers("/api/projekti/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
