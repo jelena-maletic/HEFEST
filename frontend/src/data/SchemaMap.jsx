e9e2f1fbb90b5af11cc0770edbaed02cf7b0261f
DESKTOP-KSPRDI1\User <anastasija.mitric@student.etf.unibl.org>
2026-03-06 19:48:29 +0100

    dba

---
+++ frontend/src/data/SchemaMap.jsx
@@ -0,0 +1,27 @@
+import {
+    projectSchema
+    /*materialSchema,
+    toolSchema,
+    vehicleSchema,
+    equipmentSchema,
+    assignmentSchema,
+    passwordSchema,
+    dailyReportSchema,
+    summaryReportSchema,
+    resourceRequestSchema,
+    taskSchema*/
+} from "./Forms.jsx";
+
+export const schemaMap = {
+    "projekti": projectSchema
+    /*materijal: materialSchema,
+    alat: toolSchema,
+    vozilo: vehicleSchema,
+    oprema: equipmentSchema,
+    zaduzenje: assignmentSchema,
+    lozinka: passwordSchema,
+    dnevniIzvjestaj: dailyReportSchema,
+    zbirniIzvjestaj: summaryReportSchema,
+    zahtjevResursa: resourceRequestSchema,
+    zadatak: taskSchema*/
+};
