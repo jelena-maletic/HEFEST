529cffc8647d21cb1987185e847cd1c37e83ad7f
jovana-sikanjic <sikanjic.jovanaa@gmail.com>
2026-03-16 16:40:08 +0100

    Finished CRUD for "Materijal"

--- backend/src/main/java/org/etfbl/backend/controller/MaterijalController.java
+++ backend/src/main/java/org/etfbl/backend/controller/MaterijalController.java
@@ -3,11 +3,11 @@ package org.etfbl.backend.controller;
 
 import org.etfbl.backend.dto.Materijal;
 import org.etfbl.backend.dto.RadnaOprema;
+import org.etfbl.backend.dto.Vozilo;
 import org.etfbl.backend.service.MaterijalService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,31 @@ public class MaterijalController {
     @GetMapping
     public List<Materijal> findAll() {
         return materijalService.getAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Materijal> kreirajaterijal(@RequestBody Materijal materijal) {
+        Materijal noviMaterijal = materijalService.sacuvajMaterijal(materijal);
+        return new ResponseEntity<>(noviMaterijal, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping({"/{id}"})
+    public ResponseEntity<Void> obrisiMaterijal(@PathVariable Integer id) {
+        this.materijalService.obrisiMaterijal(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Materijal> azurirajMaterijal(@PathVariable Long id, @RequestBody Materijal materijal) {
+        materijal.setId(id.toString());
+
+        Materijal azuriraniMaterijal = this.materijalService.updateMaterijal(materijal);
+        return new ResponseEntity<>(azuriraniMaterijal, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping({"/{id}"})
+    public ResponseEntity<Materijal> getById(@PathVariable Integer id) {
+        Materijal materijal = this.materijalService.getMaterijalById(id);
+        return ResponseEntity.ok(materijal);
     }
 }
--- backend/src/main/java/org/etfbl/backend/service/MaterijalService.java
+++ backend/src/main/java/org/etfbl/backend/service/MaterijalService.java
@@ -3,12 +3,18 @@ package org.etfbl.backend.service;
 import jakarta.transaction.Transactional;
 import org.etfbl.backend.dto.Materijal;
 import org.etfbl.backend.dto.RadnaOprema;
+import org.etfbl.backend.dto.Vozilo;
+import org.etfbl.backend.model.MaterijalEntity;
+import org.etfbl.backend.model.ResursEntity;
+import org.etfbl.backend.model.VoziloEntity;
 import org.etfbl.backend.repository.MaterijalRepository;
 import org.etfbl.backend.repository.RadnaOpremaRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Service
@@ -22,7 +28,48 @@ public class MaterijalService {
     }
 
     public List<Materijal> getAll() {
-        return materijal.findAll().stream().map(m -> modelMapper.map(m, Materijal.class )).toList();
+        return materijal.findAll().stream()
+                .filter(m -> !m.getObrisan())
+                .map(m -> modelMapper.map(m, Materijal.class )).toList();
 
+    }
+
+    public Materijal sacuvajMaterijal(Materijal dto) {
+        MaterijalEntity entity = modelMapper.map(dto, MaterijalEntity.class);
+        ResursEntity noviResurs = modelMapper.map(dto, ResursEntity.class);
+
+        entity.setId(noviResurs.getId());
+
+        MaterijalEntity sacuvano = materijal.save(entity);
+
+        return modelMapper.map(sacuvano, Materijal.class);
+    }
+
+    @Transactional
+    public Materijal updateMaterijal(Materijal dto){
+        MaterijalEntity entity = modelMapper.map(dto, MaterijalEntity.class);
+        Optional<MaterijalEntity> m = materijal.findById(entity.getId());
+
+        if(m.isPresent()){
+            MaterijalEntity materijalEntity = m.get();
+            BeanUtils.copyProperties(entity, materijalEntity);
+
+            return modelMapper.map(materijal.save(materijalEntity), Materijal.class);
+        }
+        else
+            throw new RuntimeException("Materijal ne postoji");
+    }
+
+    public Materijal getMaterijalById(Integer id) {
+        MaterijalEntity entity = materijal.findById(id)
+                .orElseThrow(() -> new RuntimeException("Materijal nije pronađen"));
+        return modelMapper.map(entity, Materijal.class);
+    }
+
+    public void obrisiMaterijal(Integer id) {
+        if (!materijal.existsById(id)) {
+            throw new RuntimeException("Materijal ne postoji.");
+        }
+        materijal.deleteById(id);
     }
 }
