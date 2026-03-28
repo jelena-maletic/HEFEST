8c91aa35dec72d4ecde9c1f8d96f4a706edf9d04
jovana-sikanjic <sikanjic.jovanaa@gmail.com>
2026-03-28 17:16:26 +0100

    Bug fix - status updates after one click

--- frontend/src/components/List/ListElement/ListElement.jsx
+++ frontend/src/components/List/ListElement/ListElement.jsx
@@ -55,26 +55,39 @@ export function ListElement({
             );
 
         }
-        if(binaryChoice === true){
-            return(<div className="list-element-buttons">
+        if (binaryChoice === true) {
+            return (<div className="list-element-buttons">
                 <SmallButton
                     className="nested-button"
                     type="confirm"
-                    onClickHandler={(e) => {
-                        e.stopPropagation();
+                    onClickHandler={ async (e) => {
+                        /*e.stopPropagation();
                         console.log(listElementData);
                         updateZahtjevStatus(listElementData.id, "odobren");
-                        onSuccess && onSuccess();
+                        onSuccess && onSuccess();*/
+                        e.stopPropagation();
+                        try {
+                            await updateZahtjevStatus(listElementData.id, "odobren");
+                            notify.success("Status ažuriran");
+                            if (onSuccess) await onSuccess();
+                        } catch (err) {
+                            notify.error("Greška pri ažuriranju");
                         }
+                    }
                     }
                 />
                 <SmallButton
                     className="nested-button"
                     type="deny"
-                    onClickHandler={(e) => {
+                    onClickHandler={ async (e) => {
                         e.stopPropagation();
-                        updateZahtjevStatus(listElementData.id, "neodobren");
-                        onSuccess && onSuccess();
+                        try {
+                            await updateZahtjevStatus(listElementData.id, "neodobren"); // Dodaj await
+                            notify.success("Status ažuriran");
+                            if (onSuccess) await onSuccess();
+                        } catch (err) {
+                            notify.error("Greška pri ažuriranju");
+                        }
                     }}
                 />
             </div>)
