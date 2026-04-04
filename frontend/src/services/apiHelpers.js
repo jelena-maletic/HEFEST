0ff3675991ed78583a9c2830f537ca644e381e3e
ZeljkaNikolic <User@DESKTOP-MTHOVOU>
2026-04-04 17:47:20 +0200

    Validacija za datum razduzenja i promjena za detekciju kad je kolicina resursa blizu minimalne.

--- frontend/src/services/apiHelpers.js
+++ frontend/src/services/apiHelpers.js
@@ -201,7 +201,7 @@ export const fetchData = async (tag, filterPoslovodja = false, filterTehnicar = 
             return dataArray.flatMap((t) => {
                 const oprema = [];
 
-                const lowStock = t.stanjeMagacina <= t.minimalnaKolicina * 1.1;
+                const lowStock = t.stanjeMagacina <= t.minimalnaKolicina * 1.2;
                 let temp = {
                     isLowStock: lowStock,
                     title: t.naziv,
@@ -236,7 +236,7 @@ export const fetchData = async (tag, filterPoslovodja = false, filterTehnicar = 
             return dataArray.flatMap((t) => {
                 const materijal = [];
 
-                const lowStock = t.stanjeMagacina <= t.minimalnaKolicina * 1.1;
+                const lowStock = t.stanjeMagacina <= t.minimalnaKolicina * 1.2;
                 let temp = {
                     isLowStock: lowStock,
                     title: t.naziv,
